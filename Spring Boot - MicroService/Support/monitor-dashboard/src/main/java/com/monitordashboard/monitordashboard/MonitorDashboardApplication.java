47ec170c0093ff9b0bce920c11b6e54bb57eb30a
nkhaclap <nkhaclap@vnpt.vn>
2022-07-18 22:33:17 +0700

    init

---
+++ Spring Boot - MicroService/Support/monitor-dashboard/src/main/java/com/monitordashboard/monitordashboard/MonitorDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.monitordashboard.monitordashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorDashboardApplication.class, args);
+	}
+
+}
